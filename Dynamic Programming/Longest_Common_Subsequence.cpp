8616dd2ecada8d1541df2d11943402df8a9e426a
shetty-nithin <shettynithin744@gmail.com>
2022-11-27 02:08:14 +0530

    22nd commit : 27th November 2022

--- Dynamic Programming/Longest_Common_Subsequence.cpp
+++ Dynamic Programming/Longest_Common_Subsequence.cpp
@@ -127,3 +127,34 @@ public:
         return curr[0];
     }
 };
+
+// Approach 5 : Top-Down/Tabulation
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+
+        for(int i=0; i<m+1; i++){
+            for(int j=0; j<n+1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                int ans = 0;
+                if(text1[i-1] == text2[j-1]){
+                    ans = 1 + dp[i-1][j-1];
+                }else{
+                    ans = max(dp[i-1][j], dp[i][j-1]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[m][n];
+    }
+};
--- Dynamic Programming/notes.cpp
+++ Dynamic Programming/notes.cpp
@@ -79,8 +79,8 @@
 
     Problems on unbounded Knapsack : 
         a. Rod Cutting
-        b. Coin Change
-        c. Coin Change II
+        b. Coin Change - maximum number of ways
+        c. Coin Change II - minimum number of coins
         d. Maximum Ribbon Cut
 
         Note : In 0-1 Knapsack, once the item is visited, it will be processed i.e.,
@@ -93,11 +93,197 @@
                again it will be called on n items. (only this is the difference).
         
         a. 
+            same as 0-1 knapsack but unbounded. Can use the item multiple times.
+
+        b. 
+            Relatd to subset sum problem.
+            
+            if(wt[i-1] <= j){
+                dp[i][j] = dp[i-1][j] + dp[i]]j-nums[i-1];
+            }
+            else dp[i][j] = dp[i-1][j];
+
+        c. 
+            for(int i=0; i<sum+1; i++){ // Initialization
+                for(int j=0; j<arr.size()+1, j++){
+                    if(i=0){
+                        dp[0][j] = INT_MAX;
+                    }
+                    if(j=0){
+                        dp[i][0] = 0;
+                    }
+                }
+            }
+            for(int i=2; i< ; i++){ // initialization
+                for(int j=1; j<sum+1; j++){
+                    if(j%arr[0] == 0){
+                        dp[i][j] = j/arr[0];
+                    }
+                    else {
+                        dp[i][j] = INT_MAX-1;
+                    }
+                }
+            }
+
+            if(wt[i-1] <= j){
+                dp[i][j] = min(1 + dp[i][j-arr[i+1]], dp[i-1][j]);
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
 
 
+5. Longest Common Subsequence
+    i. Longest Common Substring
+    ii. Print LCS
+    iii. Shortest common supersequence
+    iv. Print SCS
+    v. Minimum number of insertion and deletions from A to B
+    vi. Longest Repeating subsequence
+    vii. Length of longest subsequence of A which is substring in B
+    viii. Subsequence pattern matching
+    ix. Count how many times A appear as subsequence in B
+    x. Longest palindromic subsequence
+    xi. Longest palindromic substring
+    xii. Count of palindromic substring
+    xiii. Minimum number of deletion in a string to make it a palindrome.
+    xiv. Minimum number of insertion to a string to make it a palindrome.
 
 
+    Problems : 
+    LCSubsequence
+        Note : subsequence --> can be discontinuous
+               substring ---> must be continuous
 
+        int ans = 0;
+        if(text1[i] == text2[j]){
+            ans = 1 + longestCommonSubsequenceUtil(text1, text2, i+1, j+1);
+        }else{
+            int i_next = longestCommonSubsequenceUtil(text1, text2, i+1, j);
+            int j_next = longestCommonSubsequenceUtil(text1, text2, i, j+1);
+            ans = max(i_next, j_next);
+        }
+        return ans;
+
+    i.  LCSubstring
+            if matching make dp[i][j] = 1 + dp[i-1][j-1]
+            else save the longest ans till now and make the dp[i][j] = 0;  
+
+    ii. Print LCSequence
+              0     1
+            ___________
+            |    |    |
+        0   |____|____| 
+            |    |    |
+        1   |____|____|
+            
+
+            To fill the matrix:
+                if(str1[1][0] == str2[0][1]){
+                    dp[1][1] = 1 + dp[0][0];
+                }else{
+                    dp[1][1] = max(dp[1][0], dp[0][1]);
+                }
+
+            To print the LCSequence:
+                string str = "";
+                
+                int m = i;
+                int n = j;
+                while(i >= 0 && j >= 0){
+                    if(str1[i-1] == str2[j-1]){
+                        str.push_back(str1[i-1]);
+                    }
+                    else{
+                        if(dp[i][j-1] > dp[i-1][j]){
+                            j--;
+                        }else{
+                            i--;
+                        }
+                    }
+                }
+                return reverse(str);
+
+    iii.Shortest common supersequence
+        (len(str1) + len(str2)) - LCSubsequence
+
+    iv. Print Shortest Common Supersequence
+                      0     1
+            ___________
+            |    |    |
+        0   |____|____| 
+            |    |    |
+        1   |____|____|
+            
+
+            To fill the matrix:
+                if(str1[1][0] == str2[0][1]){
+                    dp[1][1] = 1 + dp[0][0];
+                }else{
+                    dp[1][1] = max(dp[1][0], dp[0][1]);
+                }
+
+            To print the SCSupersequence:
+                string str = "";
+                
+                int m = i;
+                int n = j;
+                while(i >= 0 && j >= 0){
+                    if(str1[i-1] == str2[j-1]){
+                        str.push_back(str1[i-1]);
+                    }
+                    else{
+                        if(dp[i][j-1] > dp[i-1][j]){
+                            str.push_back(str1[i-1]);
+                            j--;
+                        }else{
+                            str.push_back(str2[j-1]);
+                            i--;
+                        }
+                    }
+                }
+                while(i > 0){
+                    str.push(str1[i-1]);
+                    i--;
+                }
+                while(j > 0){
+                    str.push(str2[j-1]);
+                    j--;
+                }
+                return reverse(str);
+
+        
+
+    v.  Minimum number of insertion and deletions from A to B
+        Convert A to LCSubsequence ---> will get number of deletion
+        Convert LCSubsequence to B =--> will get number fo insertion.
+
+----------------------------------
+    Pattern Matching Algorithm to apply LCSubsequence:
+        i/p    question     o/p
+----------------------------------
+
+    viii. Subsequence pattern matching
+        str1 == str2
+        first row = first col = 0;
+        
+        if(str1[i-1] == str2[j-1] && i != j){ // with restriction (i != j)
+            dp[i][j] = 1 + dp[i-1][j-1];
+        }
+        else{
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+        }
+
+
+    Palindromes
+    x.  Longest palindromic subsequence
+        A   -----> A
+        B  ------> reverse(A)
+        Longest Palindromic Subsequence is the LCSubsequence of A & B. 
+        
+    xiii. Minimum number of deletion in a string to make it a palindrome.
+        Find Longest Palindromic Subsequence(LPS) of a gien string str.
+        return len(str)-LPSubsequence      
 
 
 
